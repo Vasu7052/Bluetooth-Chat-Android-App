03792bde7563e9059a98b62437f7782d2314fbf7
Vasu Gupta <androfly.official@gmail.com>
2018-01-14 17:35:58 +0530

    Added Toolbar Buttons

--- app/src/main/java/com/example/vasu/bluetoothchat/MainActivity.java
+++ app/src/main/java/com/example/vasu/bluetoothchat/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.vasu.bluetoothchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.connect_scan:
+                return true;
+            case R.id.discoverable:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
